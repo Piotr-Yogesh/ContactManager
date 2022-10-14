248ea52290a90e8cb76941af9deb8ea2cadc4fac
Yogesh Adhikari <yogeshadhikari81@gmail.com>
2022-10-14 13:22:28 -0500

    contact method

--- src/Contact.java
+++ src/Contact.java
@@ -1,10 +1,27 @@
 public class Contact {
 
-    private String name;
+    private String firstName;
+    private String lastName;
     private Integer phoneNumber;
 
+    Contact(String name, Integer phoneNumber){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
 
-    public Contact(){
+    public String getLastName(){
+        return lastName;
+    }
 
+    public Integer getPhoneNumber(){
+        return phoneNumber;
+    }
+
+    public String contactInfo(){
+        return firstName + " " + lastName + "  " + phoneNumber;
     }
 }
---
+++ src/ContactTesting.java
@@ -0,0 +1,5 @@
+public class ContactTesting {
+    public static void main(String[] args) {
+
+    }
+}
